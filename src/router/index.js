1c75db60bcfc7fbd0df8e7206f527444cf6210a6
Caleb <caleb.ziegler@gmail.com>
2018-06-26 16:39:12 -0500

    largeBarChart works as a vue component. Still need to componentize colors. Still seeing warnings in log.

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Manning from '@/components/manning'
+import test from '@/components/manning_test'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'manning',
       component: Manning 
+    },
+    {
+      path: '/test',
+      name: 'manning_test',
+      component: test 
     }
   ]
 })
