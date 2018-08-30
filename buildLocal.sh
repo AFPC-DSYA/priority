218927df6eb28c446fd44f0b77c69ee530a27150
Caleb <caleb.ziegler@gmail.com>
2018-08-30 17:57:22 -0500

    Adds executables for web and local build processes.

--- buildLocal.sh
+++ buildLocal.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
-yarn run build;
+yarn run build:local;
 rm ~/priority/recent_commit.txt
 rm ~/priority/version.txt
 git log -n 1 --oneline > ~/priority/recent_commit.txt
-git tag -n1 > ~/priority/version.txt
+git tag -n1 > ~/priority/version.txt 
 rm -r ~/priority/dist/data;
-rm ~/priority/dist/static/js/*.map;
 rm ~/priority/dist/static/*.png;
-cd ~/priorityProd/ 
+cd ~/priorityLocal/ 
 cp -r ~/priority/dist/* .
 cp ~/priority/version.txt .
 git add . ;
